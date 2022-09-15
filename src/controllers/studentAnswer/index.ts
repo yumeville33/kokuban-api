4dc34d0e717f8a7f89bc7c72cfb5353cb334e709
Zomer <zomerzxc.19@gmail.com>
2022-09-15 23:09:03 +0800

    Fixed image upload

--- src/controllers/studentAnswer/index.ts
+++ src/controllers/studentAnswer/index.ts
@@ -1,11 +1,7 @@
 import { NextFunction, Response, RequestHandler, Request } from "express";
-// import randomstring from "randomstring";
 
-// import { catchAsync } from "../../utils/catchAsync";
 import { AppError } from "../../utils/responses/error";
-// import { IStudentAnswer, StudentAnswer } from "../../models";
 import { StudentAnswer } from "../../models";
-import { base64ToBuffer, bufferToBase64 } from "../../utils/imageBuffer";
 
 export const createStudentAnswer: RequestHandler =
   // eslint-disable-next-line consistent-return
@@ -13,45 +9,11 @@ export const createStudentAnswer: RequestHandler =
     try {
       const { boardId } = req.params;
 
-      const {
-        content,
-        image,
-        boardOwner,
-        studentName,
-        schoolName,
-        studentSection,
-      } = req.body;
-
-      const newContent = content.map((item: any) => {
-        const newItem = { ...item };
-
-        if (newItem.toolType === "image") {
-          return {
-            ...newItem,
-            image: {
-              uri: base64ToBuffer(newItem.image.uri),
-              extensionType: newItem.image.extensionType,
-            },
-          };
-        }
-        return newItem;
-      });
-
-      const studentAnswerInfo = {
-        content: newContent,
-        image: {
-          uri: base64ToBuffer(image.uri),
-          extensionType: image.extensionType,
-        },
+      const studentAnswer = await StudentAnswer.create({
+        ...req.body,
         boardRef: boardId,
-        boardOwner,
         grade: 0,
-        studentName,
-        schoolName,
-        studentSection,
-      };
-
-      const studentAnswer = await StudentAnswer.create(studentAnswerInfo);
+      });
 
       res.status(201).json({
         status: "success",
@@ -77,42 +39,10 @@ export const getAllStudentAnswersOnBoard: RequestHandler = async (
       image: { $ne: null },
     }).sort({ updatedAt: -1 });
 
-    const newStudentAnswers: any = studentAnswers.map((item) => {
-      const newItem = {
-        _id: item._id,
-        boardOwner: item.boardOwner,
-        boardRef: item.boardRef,
-        content: item.content.map((c: any) => {
-          if (c.toolType === "image") {
-            return {
-              ...c,
-              image: {
-                uri: bufferToBase64(c.image.uri, c.image.extensionType),
-                extensionType: c.image.extensionType,
-              },
-            };
-          }
-          return c;
-        }),
-        createdAt: item.createdAt,
-        updatedAt: item.updatedAt,
-        grade: item.grade,
-        schoolName: item.schoolName,
-        studentName: item.studentName,
-        studentSection: item.studentSection,
-        image: {
-          uri: bufferToBase64(item.image!.uri, item.image!.extensionType),
-          extensionType: item.image!.extensionType,
-        },
-      };
-
-      return newItem;
-    });
-
     return res.status(200).json({
       status: "success",
       data: {
-        studentAnswers: newStudentAnswers,
+        studentAnswers,
       },
     });
   } catch (error: any) {
@@ -132,26 +62,10 @@ export const getAllStudentAnswersOnBoardTable: RequestHandler = async (
       boardRef: boardId,
     }).sort({ updatedAt: -1 });
 
-    const newStudentAnswers: any = studentAnswers.map((item) => {
-      const newItem = {
-        _id: item._id,
-        boardOwner: item.boardOwner,
-        boardRef: item.boardRef,
-        createdAt: item.createdAt,
-        updatedAt: item.updatedAt,
-        grade: item.grade,
-        schoolName: item.schoolName,
-        studentName: item.studentName,
-        studentSection: item.studentSection,
-      };
-
-      return newItem;
-    });
-
     return res.status(200).json({
       status: "success",
       data: {
-        studentAnswers: newStudentAnswers,
+        studentAnswers,
       },
     });
   } catch (error: any) {
@@ -178,44 +92,10 @@ export const getOneStudentAnswersOnBoard: RequestHandler = async (
       );
     }
 
-    const newData: any = {
-      image: {
-        uri: bufferToBase64(data.image!.uri, data.image!.extensionType),
-        extensionType: data.image!.extensionType,
-      },
-      content: data.content.map((item: any) => {
-        if (item.toolType === "image") {
-          return {
-            id: item.id,
-            toolType: item.toolType,
-            size: item.size,
-            originalSize: item.originalSize,
-            image: {
-              uri: bufferToBase64(item.image.uri, item.image.extensionType),
-              extensionType: item.image.extensionType,
-            },
-            position: item.position,
-            // eslint-disable-next-line no-underscore-dangle
-            _id: item._id,
-          };
-        }
-        return item;
-      }),
-      createdAt: data.createdAt,
-      updatedAt: data.updatedAt,
-      grade: data.grade,
-      schoolName: data.schoolName,
-      studentName: data.studentName,
-      studentSection: data.studentSection,
-      _id: data._id,
-      boardOwner: data.boardOwner,
-      boardRef: data.boardRef,
-    };
-
     return res.status(200).json({
       status: "success",
       data: {
-        studentAnswer: newData,
+        studentAnswer: data,
       },
     });
   } catch (error) {
